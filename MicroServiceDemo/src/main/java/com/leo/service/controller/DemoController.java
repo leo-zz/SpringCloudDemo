267ac601882bf0c65f71c80df6767ed0fd691c74
leo-zz <libing@zih718.com>
2018-11-21 21:50:56 +0800

    add timeconsume method for ad test

--- MicroServiceDemo/src/main/java/com/leo/service/controller/DemoController.java
+++ MicroServiceDemo/src/main/java/com/leo/service/controller/DemoController.java
@@ -6,24 +6,45 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 
 @RequestMapping("/test")
 @RestController
 public class DemoController {
+
+    Logger logger= Logger.getLogger("DemoController");
     //从配置文件中读取实例名称
     @Value("${eureka.instance.instance-id}")
     String instanceID;
 
     @PostMapping("/hello/{name}")
-    public String demoTest1(@PathVariable(value = "name") String name,
+    public String sayHello(@PathVariable(value = "name") String name,
                             @RequestParam(value = "from") String user){
-        return "Hello "+name+",this is DemoTest.From "+user+"@："+instanceID+".";
+        String content="Hello "+name+",this is DemoTest.From "+user+"@："+instanceID+".";
+        logger.log(Level.INFO,content);
+        return content;
     }
+
+    @GetMapping("/timeconsume/{length}")
+    public String timeConsuming(@PathVariable(value = "length") int length){
+        try {
+            Thread.sleep(length);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        String content="Successfully sleep "+length+" ms.";
+        logger.log(Level.INFO,content);
+        return content;
+    }
+
     @PostMapping("/upload")
-    public String demoTest2(@RequestParam("file") MultipartFile file){
+    public String uploadFile(@RequestParam("file") MultipartFile file){
         String str="file名称："+file.getOriginalFilename()+"，file大小："+file.getSize()+"From "+instanceID+".";
-        System.out.println(str);
+        logger.log(Level.INFO,str);
         return str;
     }
+
+
 }
